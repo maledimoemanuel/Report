61fb27cb1bba9e6e29f86311ebd01d5f6529626d
maledimoemanuel <105131542+maledimoemanuel@users.noreply.github.com>
2022-11-15 03:51:59 +0200

    Add files via upload

---
+++ Constants/color.js
@@ -0,0 +1,20 @@
+export default{
+    primaryColor: '#0B0E14',
+    accentColor: 'black',
+    buttonColor: 'white'
+}
+
+export const colors = {
+    buttons:"#0B0E14",
+    grey1: "#43484d",
+    grey2: "#5e6972",
+    grey3: "#86939e",
+    grey4: "#bdc6cf",
+    grey5: "#e1e8ee",
+    CardCommend: "#86939e",
+    cardBackground: 'white',
+    statusbar: "#7DF9FF",
+    iconButton: '#86939e',
+    button: '#0B0E14'
+}
+
